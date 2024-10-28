a12b582a7a4e2400dc85741d5f0b306267eb6d9a
Cameron Kuo <jienfu926@gmail.com>
2024-10-29 02:36:04 +0800

    feat(web): Add SVG sprite and IconSprite component

---
+++ web/src/components/IconSprite.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+import defaultMap from "@/assets/images/sprites.svg";
+
+defineProps<{
+  id: string;
+  map?: string;
+}>();
+</script>
+
+<template>
+  <svg class="aspect-square">
+    <use :href="`${map ?? defaultMap}#${id}`" />
+  </svg>
+</template>
