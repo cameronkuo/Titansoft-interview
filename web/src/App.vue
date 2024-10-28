a43881ed1116cec58160e6fbfc6e403c4c2f597d
Cameron Kuo <jienfu926@gmail.com>
2024-10-28 20:08:47 +0800

    feat: Add tailwindcss dependency

--- web/src/App.vue
+++ web/src/App.vue
@@ -3,8 +3,8 @@ import { RouterLink, RouterView } from 'vue-router'
 </script>
 
 <template>
-  <header>
-    <nav>
+  <header class="sticky top-0 w-full">
+    <nav class="flex items-center justify-center gap-5">
       <RouterLink to="/">Home</RouterLink>
       <RouterLink to="/profile">Profile</RouterLink>
     </nav>
