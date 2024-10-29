29eff8a76c95203a2314ae09ab60de9c7aed0ddb
Cameron Kuo <jienfu926@gmail.com>
2024-10-29 11:54:36 +0800

    feat(web): Update DefaultLayout.vue to add profile link in header and fetch user if not exist

--- web/src/layouts/DefaultLayout.vue
+++ web/src/layouts/DefaultLayout.vue
@@ -1,47 +1,27 @@
 <script setup lang="ts">
-import { useAuthStore } from '@/stores/auth'
-import { ElDropdown, ElDropdownItem, ElDropdownMenu, ElMessage, ElMessageBox } from 'element-plus'
-import { RouterLink, RouterView, useRoute, useRouter } from 'vue-router'
+import { RouterLink, RouterView } from 'vue-router'
 
-const router = useRouter()
-const route = useRoute()
+import IconSprite from '@/components/IconSprite.vue'
+import { getCookie } from '@/libs/cookie'
+import { useAuthStore } from '@/stores/auth'
+
 const authStore = useAuthStore()
 
-const handleLogout = () => {
-  ElMessageBox.confirm('確定要登出嗎？', '登出', {
-    confirmButtonText: '確定',
-    cancelButtonText: '取消',
-    type: 'warning',
-  }).then(() => {
-    authStore.logout()
-    if (route.meta.auth) {
-      router.replace({ name: 'VerifyView', query: { redirect: route.fullPath } })
-    }
-  })
+if (!authStore.user && getCookie('token')) {
+  authStore.getUserInfo()
 }
 </script>
 
 <template>
-  <header class="sticky top-0 flex w-full justify-between px-5 py-2">
+  <header class="sticky top-0 flex w-full justify-between px-10 py-2">
     <nav class="flex items-center justify-center gap-5">
       <RouterLink to="/" class="text-inherit">Home</RouterLink>
-      <RouterLink to="/profile" class="text-inherit">Profile</RouterLink>
     </nav>
-    <template v-if="authStore.user">
-      <ElDropdown>
-        <button class="flex items-center gap-2 border-none bg-transparent p-0 dark:text-white">
-          <img v-if="authStore.user?.photo" :src="authStore.user.photo" class="h-10 w-10 rounded-full object-cover object-top" />
-          <small>Hi, {{ authStore.user?.username ?? '訪客' }}</small>
-        </button>
-        <template #dropdown>
-          <ElDropdownMenu>
-            <ElDropdownItem @click="handleLogout">登出</ElDropdownItem>
-          </ElDropdownMenu>
-        </template>
-      </ElDropdown>
-    </template>
+    <RouterLink :to="{ name: 'ProfileView' }" class="flex items-center gap-2 whitespace-nowrap border-none bg-transparent p-0 no-underline dark:text-white">
+      <img v-if="authStore.user?.photo" :src="authStore.user.photo" class="h-10 w-10 rounded-full object-cover object-top" />
+      <IconSprite v-else id="person" class="w-10 dark:text-gray-400" />
+      <small>Hi, {{ authStore.user?.username ?? '訪客' }}</small>
+    </RouterLink>
   </header>
   <RouterView />
 </template>
-
-<style lang="scss" scoped></style>
